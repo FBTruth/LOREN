30823da37d94558c60136080642f98176021d742
Roman Scott <roman@burtonscott.com>
2022-07-28 18:14:22 -0700

    Added HTTP server to LOREN to respond to POST requests

---
+++ serve.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from src.loren import Loren
+from nltk.tokenize import sent_tokenize
+from huggingface_hub import snapshot_download
+
+import os
+import torch
+
+model = snapshot_download('Jiangjie/loren')
+config = {
+    "n_gpu": torch.cuda.device_count(),
+    "model_type": "roberta",
+    "model_name_or_path": "roberta-large",
+    "logic_lambda": 0.5,
+    "prior": "random",
+    "mask_rate": 0.0,
+    "cand_k": 3,
+    "max_seq1_length": 256,
+    "max_seq2_length": 128,
+    "max_num_questions": 8,
+    "do_lower_case": False,
+    "seed": 42,
+    'fc_dir': os.path.join(model, 'fact_checking/roberta-large/'),
+    'mrc_dir': os.path.join(model, 'mrc_seq2seq/bart-base/'),
+    'er_dir': os.path.join(model, 'evidence_retrieval/')
+}
+
+loren = Loren(config, verbose=False)
+app = Flask(__name__)
+
+@app.route('/check', methods=['POST'])
+def json_example():
+    req = request.get_json()
+    text = req['text']
+    claims = []
+
+    for sentence in sent_tokenize(text):
+        js = loren.check(sentence)
+        claims.append({
+            'claim': js['claim'],
+            'truth': js['claim_veracity'],
+            'evidence': js['evidence']
+        })
+
+    return jsonify({ 'claims': claims })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
